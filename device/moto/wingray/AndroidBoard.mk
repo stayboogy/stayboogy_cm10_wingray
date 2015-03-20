0ed2addb0b8c2900ddc17d3017d5f4388ba39142
M <m@i5.(none)>
2015-03-19 21:04:35 -0500

    fix make files

--- device/moto/wingray/AndroidBoard.mk
+++ device/moto/wingray/AndroidBoard.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-TARGET_KERNEL_CONFIG := cyanogenmod_stingray_defconfig
+TARGET_KERNEL_CONFIG := stayboogy_wingray_defconfig
 
-TARGET_KERNEL_SOURCE := kernel/motorola/stingray
+TARGET_KERNEL_SOURCE := kernel/moto/wingray-bigpart
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
@@ -14,18 +14,3 @@ ALL_PREBUILT += $(file)
 $(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
 
-include $(CLEAR_VARS)
-
-COMMON_DIR := vendor/nvidia/common/
-
-ifeq ($(wildcard $(COMMON_DIR)/TegraBoard.mk),$(COMMON_DIR)/TegraBoard.mk)
-include $(COMMON_DIR)/TegraBoard.mk
-endif
-
-subdir_makefiles:= \
-	$(LOCAL_PATH)/libaudio/Android.mk \
-	$(LOCAL_PATH)/taudio/Android.mk
-
-include $(subdir_makefiles)
-
--include vendor/motorola/stingray/AndroidBoardVendor.mk
